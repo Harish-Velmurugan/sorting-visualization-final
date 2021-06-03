052825e2ac3e9f7ef402a4604cb3b1abde01587e
Harish Velmurugan <aimbot862@gmail.com>
2021-06-03 18:47:45 +0530

    Final Commit

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">React;</span> by
+          Harish, Omar, Basheer, adaikal
+        
+      </section>
+
+      
+    </footer>
+  );
+};
+
+export default Footer;
